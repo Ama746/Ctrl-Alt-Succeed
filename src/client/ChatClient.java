c50f5e35e8a3976c662881cb1984767be761bf52
zunehra <zunehra.irfan@outlook.com>
2025-11-20 14:33:16 +0000

    level 2 chnages

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -2,34 +2,44 @@ package client;
 
 import java.io.*;
 import java.net.*;
+import javax.net.ssl.SSLSocket; // replacing serversocket with ssl
+import javax.net.ssl.SSLSocketFactory;
 
 public class ChatClient {
 	private static final int PORT = 1234;
+	private static final String SERVER_IP = "localhost";
 
 	public static void main(String[] args){
-		try(Socket socket = new Socket("localhost", PORT); //creates a network connection to server via local and port 1234 - throw exception if unreachable
+		
+		//tells java which  truststore for client to trust server certificate 
+		System.setProperty("javax.net.ssl.trustStore", "client.truststore");
+		System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+		
+		//creating SSL  socket
+		SSLSocketFactory ssf= (SSLSocketFactory) SSLSocketFactory.getDefault();
+		
+		try(SSLSocket socket = (SSLSocket) ssf.createSocket(SERVER_IP,PORT); //creates a network connection to server via local and port 1234 - throw exception if unreachable
 				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//read byte messages from server
 				PrintWriter out = new PrintWriter(socket.getOutputStream(), true); //write/send messages to server - e.g. broadcast msgs from others
 				BufferedReader input = new BufferedReader(new InputStreamReader(System.in))
 						) { 
-			System.out.println("Server connected. Type here:");
+			System.out.println("Server connected over SSL . Type here:");
 			
 			//thread to listen for msgs sent from server
-		Thread thread = new Thread() { 
-			@Override
-			public void run() {
+		Thread thread = new Thread(() -> { 
+			
 			try {
 				String serverMsg; //= in.readLine();// read one line from server //c
 				while ((serverMsg = in.readLine()) != null) { //keep reading until server closes
-					System.out.println("Message: " + serverMsg); //c 
+					System.out.println(serverMsg); //c 
 					} //if (serverMsg == null) println("Server disconnected"); > 
 				System.out.println("Server disconnected.");
 				} catch (IOException e) {
 					//System.out.println("Error from server"); //confirmed
-					e.printStackTrace();
+					System.out.println("connection lost: " + e.getMessage());
 				}
-			}
-		};
+			
+		});
 		thread.start(); //starts listening on server along with msgs from client	
 			
 		//input from client on console and sends to server
@@ -43,7 +53,8 @@ public class ChatClient {
 			}
 			
 		} catch (IOException e)	{
-		e.printStackTrace();
+		 System.out.println("unable to securly connect to server: ");
+		 e.printStackTrace();
 		}
 	}
 }
