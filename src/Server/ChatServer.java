0057af9be945e73fa3cd043e58f08a6596ef6039
zunehra <zunehra.irfan@outlook.com>
2025-11-27 16:53:48 +0000

    chnages keystore and truststore location no bin in gitignore

--- src/Server/ChatServer.java
+++ src/Server/ChatServer.java
@@ -15,7 +15,7 @@ private static Set<ClientHandler> clientHandlers = Collections.synchronizedSet(n
 public static void main(String[] args) {
 	
 	//tells java which keystore and password for ssl
-	System.setProperty("javax.net.ssl.keyStore", "server.keystore");
+	System.setProperty("javax.net.ssl.keyStore", "C:\stores");
 	System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
 	
 	System.out.println("Chat server started");
