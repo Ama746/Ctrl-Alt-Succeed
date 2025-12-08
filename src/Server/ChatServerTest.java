3404b8e5a6b0a1e6a0ad09ed24e55c0560d53e94
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-12-08 00:29:46 +0000

    updated ChatServerTest for level 3

--- src/Server/ChatServerTest.java
+++ src/Server/ChatServerTest.java
@@ -1,4 +1,4 @@
-package Server; //fake client to test server connection v1.1
+package Server; //fake client to test server connection v2 (Level 3)
 
 import java.io.*; //input/output
 import javax.net.ssl.SSLSocketFactory;
@@ -16,7 +16,7 @@ public class ChatServerTest {
 		System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
 
 		System.out.println("Certificate confirmed in truststore.");
-		System.out.println("Starting SSL client test for Level 2 on port: " + PORT); //print confirms certificate linked and test starting
+		System.out.println("Starting SSL client test for Level 3 on port: " + PORT); //print confirms certificate linked and test starting
 		
 		try {
 			//Create SSL socket
@@ -29,13 +29,27 @@ public class ChatServerTest {
             	//read and send messages
             	String serverMsg = in.readLine(); //read initial server welcome message "Welcome Enter name:"
             	System.out.println("Server: " + serverMsg);
-            	out.println("Tester"); //send test username to server
-            	out.println("Hello from the Level 2 Test Client."); //send message to server
+            	out.println("Tester-Level3"); //send test username to server
+            	out.println("Hello from the Level 3 Test Client."); //send message to server
             	System.out.println("Message sent.");
             	
+				//spam large msgs
+				System.out.println("Testing 100-character limiter control by sending long messages.");
+				String longMsg = "xoxo".repeat(150);
+				for (int i = 1; i <= 5; i++) { //send 5 long msgs
+					out.println(longMsg);
+				}
+				
+				//spam msgs quickly with no delay
+				System.out.println("Testing rate-limiter controls by sending rapid messages.");
+				for (int i = 1; i <=5; i++) { //send 5 quick msgs
+					out.println("Quick message " + i);
+				}
+
             	String receivedMsg;
             	while ((receivedMsg = in.readLine()) !=null) {
-            		System.out.println("Received: " + receivedMsg);
+            		System.out.println("Server: " + receivedMsg);
+					if (receivedMsg.contains("disconnected"))
             		break; //exit after response
             	}
             	
@@ -43,18 +57,14 @@ public class ChatServerTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("SSL test completed for Level 2!");
+		System.out.println("Spam sent. Test completed for Level 3!");
 	}
 }
 /*Notes:
  * Tester needs to use same keystore > pointing truststore to same file.
- * Test Client for Level 2 also uses SSLSocketFactory instead of Socket to establish a connection.
- * It follows a simple flow of reading the welcome message sent by the server ("Welcome Enter name:"), sending a username back and then a message.
- * Version 1 doesn't handle multiple messages, continuous chatting and only reads one broadcast message from the server before exiting.
- * A few limitations: Version 1 doesn't handle multiple messages, continuous chatting and only reads one broadcast then exits.
- * Plan to upgrade and convert to JUnit test so it can automatically run rather than manually.
- * To Do: try stress testing via spawning multiple clients
- * 
+ * Included spam tests:
+ * fast messages to trigger rate-limiter
+ * long messages to trigger 100 character limiter
  * Other:
  * client.truststore contains trusted certificates
  * server.keystore contains server's private key
