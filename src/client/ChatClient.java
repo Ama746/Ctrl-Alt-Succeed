eac9949098990491937ab63b5a1f83c2f22c91d8
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-12-08 13:11:56 +0000

    changed ip to reflect server

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -6,7 +6,7 @@ import javax.net.ssl.SSLSocketFactory; // replacing serversocket with ssl
 
 public class ChatClient {
 	private static final int PORT = 1234;
-	private static final String SERVER_IP = "192.168.56.1";
+	private static final String SERVER_IP = "172.20.10.2";
 
 	public static void main(String[] args){
 		
